989e0ed3a3be8deaaca996c31c003129954ed6fb
Davies Magare <davieskamanda@gmail.com>
2023-11-07 11:08:27 +0000

    Task 4: Recreate an instance from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -3,17 +3,26 @@
 Base Model Module 
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """BaseModel Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor Method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    date = datetime.fromisoformat(value)
+                    setattr(self, key, date)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """Returns string representation of the object"""
@@ -21,7 +30,7 @@ class BaseModel:
 
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
