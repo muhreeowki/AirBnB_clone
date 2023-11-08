0f17e664874f31a372add2c8364a998d74c2f644
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-08 11:10:21 +0300

    Finishing with task 5: Fixing import errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -22,10 +22,6 @@ class FileStorage:
     def save(self):
         """Serialize the objects dictionary into a json file."""
         with open(self.__file_path, "w") as f:
-            # New dictionary to hold dictionary representaton of the objects
-            # Convert each object into its dictionary representaton
-            # Store it in the new dictionary
-            # Convert the new dictionary into a json string and write that string to the file
             obj_dicts = {}
             for key, value in self.__objects.items():
                 obj_dicts[key] = value.to_dict()
@@ -35,22 +31,10 @@ class FileStorage:
         """Deserialize the json file into the objects dictionary."""
         try:
             with open(self.__file_path, "r") as f:
-                # Load the dictionary of object dicts from the file
-                # Convert each object dict into an object
-                # Store each object in the __objects dictionary
                 json_string = f.read()
                 if json_string:
                     obj_dicts = json.loads(json_string)
                     for key, value in obj_dicts.items():
                         self.__objects[key] = BaseModel(**value)
-                        """
-                        Main dictionary {
-                            BaseModel.1212: {att:val}
-                            BaseModel.8232: {att:val}
-                            BaseModel.1211: {att:val}
-                            BaseModel.3222: {att:val}
-                            BaseModel.0292: {att:val}
-                        }
-                        """
         except FileNotFoundError:
             pass
