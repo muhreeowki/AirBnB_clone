a24ad81b5cb1614fd911080e32217a25d90b7dca
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-08 20:09:38 +0300

    Task 7: Fixing the all method

--- console.py
+++ console.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
-
-
 """
 The hbnb console
 """
@@ -9,51 +6,32 @@ import cmd
 from models.base_model import BaseModel
 import models
 
+
 class HBNBCommand(cmd.Cmd):
     """Implements a command interpreter"""
 
-    prompt = '(hbnb) '
-    
-    def validate_input(self, model_id):
-        """Validates interpreter input"""
-        if model_id:
-            model_list = model_id.split(' ', 1)
-            if model_list[0] in globals():
-                if len(model_list) >= 2:
-                    model_id = model_id.replace(" ", ".")
-                    objects = models.storage.all()
-                    if model_id in objects.keys():
-                        return True
-                    else:
-                        print("** no instance found **")
-                else:
-                    print("** instance id missing **")
-            else:
-                print("** class doesn't exist **")
-        else:
-            print("** class name missing **")
-        return False
-    
-    def do_all(self, class_name=None):
+    prompt = "(hbnb) "
+
+    def do_all(self, class_name):
         """
         Prints string representation of all instances
         based on class_name
         """
-        objects = models.storage.all()
-        obj_list = []
-        if class_name is not None:
-            for key, value in objects.items():
-                if value.__class__.__name__ == class_name:
-                    obj_list.append(str(value))
+        if class_name in globals() or not class_name:
+            objects = models.storage.all()
+            obj_list = []
+            for key in objects.keys():
+                if class_name:
+                    if objects[key].__class__.__name__ == class_name:
+                        obj_list.append(str(objects[key]))
+                else:
+                    obj_list.append(str(objects[key]))
+            print(obj_list)
         else:
-            for key, value in objects.items():
-                obj_list.append(str(value))
-
-        print(obj_list)
+            print("** class doesn't exist **")
 
     def do_create(self, class_name):
         """Validates the classname and creates new instance"""
-
         if class_name:
             if class_name in globals():
                 obj = eval("{}()".format(class_name))
@@ -79,17 +57,13 @@ class HBNBCommand(cmd.Cmd):
             del objects[model_id]
             models.storage.save()
 
-
-    def do_EOF(self):
+    def do_EOF(self, line):
         """Exits the interpreter"""
-
         return True
 
     def do_quit(self, line):
         """Exits the interpreter"""
-
         return True
-
 
     def emptyline(self):
         """Handles empty lines"""
@@ -98,6 +72,26 @@ class HBNBCommand(cmd.Cmd):
     def postloop(self):
         print()
 
+    def validate_input(self, model_id):
+        """Validates interpreter input"""
+        if model_id:
+            model_list = model_id.split(" ", 1)
+            if model_list[0] in globals():
+                if len(model_list) >= 2:
+                    model_id = model_id.replace(" ", ".")
+                    objects = models.storage.all()
+                    if model_id in objects.keys():
+                        return True
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+        return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
